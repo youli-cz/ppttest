8a440fd1b27220e830104f067f0b024afd8a2202
cz <1494907690@qq.com>
2021-07-05 16:41:21 +0800

    创建pptcontroller

---
+++ src/main/java/com/cz/ppttest/controller/PPTController.java
@@ -0,0 +1,22 @@
+package com.cz.ppttest.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * description
+ *
+ * @author pisx 2021/07/05 16:33
+ */
+
+@RestController
+@RequestMapping("/PPT")
+public class PPTController {
+
+    @GetMapping("/export")
+    public String exportPPT(){
+
+        return "success";
+    }
+
+
+}
